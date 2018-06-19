a3d03ce19160dad4dd4dac0fb0be7476a85435d7
calebelson <caleb.elson@gmail.com>
2018-06-18 21:24:33 -0500

    Minor fixes

--- Learn Metric/View Controllers/AllAnimationsViewController.swift
+++ Learn Metric/View Controllers/AllAnimationsViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import Foundation
 import NVActivityIndicatorView
 
 class AllAnimationsViewController: UIViewController {
@@ -21,10 +20,10 @@ class AllAnimationsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // backgroundColor not being set causes visual bugs
         skyconView.backgroundColor = .black
         skyconView.setColor = #colorLiteral(red: 0.6980392157, green: 0.8431372549, blue: 1, alpha: 1)
         skyconView.setType = .cloudy
-        skyconView.tag = 100
         allAnimationsView.addSubview(skyconView)
         
         activityIndicatorView.color = #colorLiteral(red: 0.6980392157, green: 0.8431372549, blue: 1, alpha: 1)
