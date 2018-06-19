4408500177b0bcf3f329f50926b97ae6203df56c
calebelson <caleb.elson@gmail.com>
2018-06-19 11:15:01 -0500

    Fixed animation view layout in animations view controller, random indicator view on tap

--- Learn Metric/View Controllers/AllAnimationsViewController.swift
+++ Learn Metric/View Controllers/AllAnimationsViewController.swift
@@ -13,52 +13,47 @@ class AllAnimationsViewController: UIViewController {
     
     @IBOutlet weak var allAnimationsView: UIView!
     lazy var skyconView = SKYIconView(frame: CGRect(x: 0, y: 0, width: allAnimationsView.frame.width, height: allAnimationsView.frame.height))
-    lazy var activityIndicatorView = NVActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: allAnimationsView.frame.width, height: allAnimationsView.frame.height))
+    lazy var activityIndicatorView = NVActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: allAnimationsView.frame.width, height: allAnimationsView.frame.height), color: #colorLiteral(red: 0.6980392157, green: 0.8431372549, blue: 1, alpha: 1))
     
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    // "circleStrokeSpin" is the last value listed under NVActivityIndicatorType, 1 added after random number generation to avoid .blank
+    let totalActivityViews = (NVActivityIndicatorType.circleStrokeSpin.rawValue)
+
+    override func viewDidLayoutSubviews() {
         // backgroundColor not being set causes visual bugs
         skyconView.backgroundColor = .black
         skyconView.setColor = #colorLiteral(red: 0.6980392157, green: 0.8431372549, blue: 1, alpha: 1)
         skyconView.setType = .cloudy
         allAnimationsView.addSubview(skyconView)
         
-        activityIndicatorView.color = #colorLiteral(red: 0.6980392157, green: 0.8431372549, blue: 1, alpha: 1)
-        activityIndicatorView.type = .ballClipRotatePulse
     }
-    
+        
     @IBAction func changeAnimation(_ sender: UITapGestureRecognizer) {
         
+        // Clears allAnimationsView
         skyconView.removeFromSuperview()
         activityIndicatorView.removeFromSuperview()
         
+        // 1 added here to avoid .blank
+        let randomNumber = Int(arc4random_uniform(UInt32(totalActivityViews))) + 1
+        
+        guard let activityIndicatorType = NVActivityIndicatorType(rawValue: randomNumber) else {
+            
+            activityIndicatorView.type = .pacman
+            activityIndicatorView.color = #colorLiteral(red: 0.944022473, green: 0.4014404026, blue: 0.4582167554, alpha: 1)
+            allAnimationsView.addSubview(activityIndicatorView)
+            activityIndicatorView.startAnimating()
+            
+            print("Unexpected failure setting random NVActivityIndicatorType, number: \(randomNumber)")
+            
+            return
+        }
+        
+        // Randomly selected NVActivityIndicatorType
+        activityIndicatorView.type = activityIndicatorType
+        
         allAnimationsView.addSubview(activityIndicatorView)
         activityIndicatorView.startAnimating()
+     
     }
-    
-    //        // "circleStrokeSpin" is the last value listed under NVActivityIndicatorType
-    //
-    //        let totalLoadingViews = (NVActivityIndicatorType.circleStrokeSpin.rawValue)
-    //
-    //        // Blank is 0
-    //        let randomNumber = Int(arc4random_uniform(UInt32(totalLoadingViews))) + 1
-    //
-    //
-    //        // If NVActivityIndicatorType can not be set, sets a default value colored red
-    //
-    //        guard let activityIndicator = NVActivityIndicatorType(rawValue: randomNumber) else {
-    //
-    //            loadingView.type = .pacman
-    //            loadingView.color = .purple
-    //            loadingView.startAnimating()
-    //
-    //            return
-    //        }
-    //
-    //        loadingView.type = activityIndicator
-    //        loadingView.startAnimating()
 }
 
